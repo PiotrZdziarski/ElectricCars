ec03512f76be867d28b79bbbf68cc3fbec4b9549
PiotrZdziarski <pzdziarski01@gmail.com>
2018-10-01 23:04:27 +0200

    adding fancybox and carousel preview

--- resources/views/sites/announcement.blade.php
+++ resources/views/sites/announcement.blade.php
@@ -2,8 +2,15 @@
 @section('title') XD @endsection
 
 {{--@section('specialFonts')--}}
-    {{--<link href="https://fonts.googleapis.com/css?family=Overpass:400,600,700&amp;subset=latin-ext" rel="stylesheet">--}}
+{{--<link href="https://fonts.googleapis.com/css?family=Overpass:400,600,700&amp;subset=latin-ext" rel="stylesheet">--}}
 {{--@endsection--}}
+@section('specialScripts')
+    <script src="{{asset('js/jquery.fancybox.min.js')}}" defer></script>
+@endsection
+
+@section('specialStyles')
+    <link href="{{asset('css/jquery.fancybox.min.css')}}" defer rel="stylesheet">
+@endsection
 
 @section('content')
     <announcement></announcement>
