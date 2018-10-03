fe2bde2d732a3757eb72b2f92da4938efb58bd9c
PiotrZdziarski <pzdziarski01@gmail.com>
2018-10-03 21:33:34 +0200

    adding sorting by functionality and adverts per page

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Http\Request;
 //});
 
 //adverts
-Route::get('/adverts/{per_page?}', 'AdvertsController@index');
+Route::get('/announcements/{per_page?}/{order_by?}', 'AdvertsController@index');
